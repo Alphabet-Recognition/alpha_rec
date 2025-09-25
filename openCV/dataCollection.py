fix/freeze because crop overflow and press'q' to quit camera

openCV/dataCollection.py
@@ -4,46 +4,52 @@ import numpy as np
 import math
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
+detector = HandDetector(maxHands=3)
 
 offset = 20
-imgSize = 30
+imgSize = 64
 
 while True:
     success, img = cap.read()
+    if not success:
+        break
+    
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
-        x,y,w,h = hand['bbox']
-        
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-        imgCrop = img[y-offset:y + h+offset, x-offset:x + w+offset]
-        
+        x, y, w, h = hand['bbox']
+
+        y1 = max(0, y - offset)
+        y2 = min(img.shape[0], y + h + offset)
+        x1 = max(0, x - offset)
+        x2 = min(img.shape[1], x + w + offset)
+
+        imgCrop = img[y1:y2, x1:x2]
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+
         if imgCrop.size != 0:
-            imgCropShape = imgCrop.shape
-        
-        imgWhite[0:imgCropShape[0], 0:imgCropShape[1]] = imgCrop
-        
-        aspectRatio = h/w
-        
-        if aspectRatio > 1:
-            k = imgSize/h 
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize-wCal)/2)
-            imgWhite[:, wGap:wCal+wGap] = imgResize       
-            
-        else:
-            k = imgSize/w
-            hCal = math.ceil(k*h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize-hCal)/2)
-            imgWhite[hGap:hCal+hGap, :] = imgResize         
-        
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
-    
+            aspectRatio = h / w
+
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()